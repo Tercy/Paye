8b226f749ab7b33cd3ee8c61ab33481d56764d22
daGuyu <joshua.maluleka@fnb.co.za>
2021-05-04 16:27:50 +0200

    controller

--- paye/src/main/java/com/paye/controller/PayeController.java
+++ paye/src/main/java/com/paye/controller/PayeController.java
@@ -1,4 +1,20 @@
 package com.paye.controller;
 
+import com.paye.model.PayeModel;
+import com.paye.service.PayeService;
+
+@RestController
 public class PayeController {
+    @Autowired
+    PayeService payeService;  //Service which will do all data retrieval/manipulation work
+
+
+    //-------------------Calculate tax --------------------------------------------------------
+
+    @RequestMapping(value = "/paye/", method = RequestMethod.GET)
+    public double calculateTax(@RequestBody PayeModel payeModel) {
+
+
+        return payeService.incomeTax(payeModel.salary,payeModel.year,payeModel.age,payeModel.medicalAid);
+    }
 }
